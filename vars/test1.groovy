c4c2091efef0d4c6a7376601f3b07574789b544f
Alejandra Delgado <alejandra.delgado@flare.com.co>
2025-09-22 12:20:25 -0500

    TEST -2

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,3 +1,44 @@
-def call(){
-    sh 'echo this is my first shared library test - 1'
+/**
+ * Executes a full Django deployment, including setting up a virtual environment,
+ * deploying files with rsync, and running Django management commands.
+ *
+ * @param webRoot The destination web root for deployment.
+ * @param venvName The name of the virtual environment directory.
+ */
+
+
+// Función para configurar el entorno virtual y activar el entorno Python
+def setUpEnv(webRoot, venvName) {
+    sh """
+        python3 -m venv ${webRoot}/${venvName} && \
+        . ${webRoot}/${venvName}/bin/activate && \
+        pip install -r ${webRoot}/requirements.txt
+    """
 }
+
+// Función para hacer el despliegue con rsync
+def deployingFiles(workspace, webRoot, venvName) {
+    sh """
+        rsync -rvD --delete \\
+            --exclude '${venvName}/' \\
+            --exclude '__pycache__/' \\
+            --exclude '.git/' \\
+            ${workspace}/ \\
+            ${webRoot}/
+    """
+}
+
+// Función para ejecutar comandos de Django dentro del entorno virtual
+def executeCommandsDjango(webRoot) {
+    sh """
+        . ${webRoot}/venv/bin/activate && \
+        python manage.py migrate collectstatic --no-input
+    """
+}
+
+// Función para limpiar el entorno virtual
+def cleanEnv(webRoot, venvName) {
+    sh "rm -rf ${webRoot}/${venvName}"
+}
+
+
