be94acc03fe656934cc0e5f1d52274df71f7b2d6
Alejandra Delgado <123127612+aleja032@users.noreply.github.com>
2025-09-23 11:52:47 -0500

    Update testPython.groovy

--- vars/testPython.groovy
+++ vars/testPython.groovy
@@ -9,36 +9,44 @@
 
 // Función para configurar el entorno virtual y activar el entorno Python
 def setUpEnv(webRoot, venvName) {
-    sh """
-        python3 -m venv ${webRoot}/${venvName} && \
-        . ${webRoot}/${venvName}/bin/activate && \
-        pip install -r ${webRoot}/requirements.txt
-    """
+    script {
+        sh """
+            python3 -m venv ${webRoot}/${venvName} && \
+            . ${webRoot}/${venvName}/bin/activate && \
+            pip install -r ${webRoot}/requirements.txt
+        """
+    }
 }
 
 // Función para hacer el despliegue con rsync
 def deployingFiles(workspace, webRoot, venvName) {
-    sh """
-        rsync -rvD --delete \\
-            --exclude '${venvName}/' \\
-            --exclude '__pycache__/' \\
-            --exclude '.git/' \\
-            ${workspace}/ \\
-            ${webRoot}/
-    """
+    script {
+        sh """
+            rsync -rvD --delete \\
+                --exclude '${venvName}/' \\
+                --exclude '__pycache__/' \\
+                --exclude '.git/' \\
+                ${workspace}/ \\
+                ${webRoot}/
+        """
+    }
 }
 
 // Función para ejecutar comandos de Django dentro del entorno virtual
 def executeCommandsDjango(webRoot) {
-    sh """
-        . ${webRoot}/venv/bin/activate && \
-        python manage.py migrate collectstatic --no-input
-    """
+    script {
+        sh """
+            . ${webRoot}/venv/bin/activate && \
+            python manage.py migrate collectstatic --no-input
+        """
+    }
 }
 
 // Función para limpiar el entorno virtual
 def cleanEnv(webRoot, venvName) {
-    sh "rm -rf ${webRoot}/${venvName}"
+    script {
+        sh "rm -rf ${webRoot}/${venvName}"
+    }
 }
 
 
